a420496aeadf95c01d099d6bff5724c6b97dac88
przemekkijak <kijak.przemyslaw1@gmail.com>
2023-07-01 18:23:41 +0200

    styled enginge added

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -3,41 +3,54 @@
 import React from 'react';
 import { Link } from "react-router-dom";
 import { css } from "@emotion/react"
+import {styled} from "@mui/material";
 
+const SidebarDiv = styled('div')({
+  width: '100%',
+  margin: '10px',
+  padding: '20px 0 20px 0',
+  cursor: 'pointer',
+  transition: 'background-color 0.3s ease',
+  ":hover": {
+    backgroundColor: '#ddd'
+  }
+});
 
 const Sidebar = () => {
   return (
-    <div
-      css={css`
+      <div
+          css={css`
         position: relative;
         border: 1px solid red;
         height: 100vh;
         width: 10vw;
         left: 0;
         display: flex;
+        flex-direction: column;
         align-items: center;
         justify-content: center;
       `}
-    >
-      <ul css={css`
-        margin: 0;
-        padding: 0;
-        display: flex;
-        flex-direction: column; 
-        align-items: center;
-      `}>
-        <li className="">
-          <Link to="/" className="">Dashboard</Link>
-        </li>
-        <li className="">
-          <Link to="/history" className="">History</Link>
-        </li>
-        <li className="">
-          <Link to="/logout" className="">Logout</Link>
-        </li>
-      </ul>
-    </div>
+      >
+        <SidebarDiv>
+          <Link to="/" className="">
+            Dashboard
+          </Link>
+        </SidebarDiv>
+
+        <SidebarDiv>
+          <Link to="/history" className="">
+            History
+          </Link>
+        </SidebarDiv>
+
+        <SidebarDiv>
+          <Link to="/logout" className="">
+            Logout
+          </Link>
+        </SidebarDiv>
+      </div>
   );
 };
+
 
 export default Sidebar;
